c592738edc1775b4746eb23a6eb65c8d74c672e8
Konstantinos Kaloutas <sprocketc@gmail.com>
2023-11-03 10:00:52 +0200

    improve error handling

--- src/renderer/notification/views.cljs
+++ src/renderer/notification/views.cljs
@@ -8,19 +8,22 @@
   (let [notifications @(rf/subscribe [:notifications])]
     [:div.fixed.flex.flex-col.m-4.right-0.bottom-0.gap-2.items-end
 
+     [:div.relative
+      (map-indexed
+       (fn [index notification]
+         [:div.toast
+          {:key index
+           :style {:bottom (* index 10)
+                   :right 0}}
+          [:div.toast-description
+           (:content notification)]
+          [comp/icon-button
+           "times"
+           {:title "Dismiss"
+            :on-click #(rf/dispatch [:notification/remove index])}]])
+       notifications)]
+
      (when (second notifications)
        [:button.button.level-3.p-2
         {:on-click #(rf/dispatch [:notification/clear-all])}
-        "Clear all"])
-
-     (map-indexed
-      (fn [index notification]
-        [:div.toast
-         {:key index}
-         [:div.toast-description
-          (:content notification)]
-         [comp/icon-button
-          "times"
-          {:title "Dismiss"
-           :on-click #(rf/dispatch [:notification/remove index])}]])
-      notifications)]))
+        "Clear all"])]))
--- src/renderer/utils/error.cljs
+++ src/renderer/utils/error.cljs
@@ -3,19 +3,14 @@
    [reagent.core :as ra]
    [re-frame.core :as rf]))
 
+(def error-message
+  "Your last action was canceled due to an error.")
+
 (defn boundary
   []
-  (let [error-state (ra/atom nil)]
-    (ra/create-class
-     {:component-did-catch (fn [_this error info]
-                             (reset! error-state [error info]))
-      :get-derived-state-from-error #(rf/dispatch [:history/cancel])
-      :reagent-render (fn [& children]
-                        (when @error-state
-                          [:div
-                           "Something went wrong."
-                           [:input
-                            {:type "button"
-                             :on-click #(reset! error-state nil)
-                             :value "Try again"}]])
-                        (into [:<>] children))})))
+  (ra/create-class
+   {:component-did-catch (fn [_this _error _info]
+                           (rf/dispatch [:notification/add {:content error-message}]))
+    :get-derived-state-from-error #(rf/dispatch [:history/cancel])
+    :reagent-render (fn [& children]
+                      (into [:<>] children))}))
