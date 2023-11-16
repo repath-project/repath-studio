81e315078c71e4214da36d572e19e99645117676
Konstantinos Kaloutas <sprocketc@gmail.com>
2023-11-16 09:21:46 +0200

    refactor

--- src/file.cljs
+++ src/file.cljs
@@ -13,38 +13,48 @@
 
 (def default-path (.getPath app "documents"))
 
-(def dialog-options {:defaultPath default-path
-                     ;; https://www.electronjs.org/docs/api/structures/file-filter#filefilter-object
-                     :filters [{:name "rp"
-                                :extensions ["rp"]}]})
+(def dialog-options
+  {:defaultPath default-path
+   ;; https://www.electronjs.org/docs/api/structures/file-filter#filefilter-object
+   :filters [{:name "rp"
+              :extensions ["rp"]}]})
 
 (defn save
   "Saves the provided data.
    SEE https://www.electronjs.org/docs/api/dialog#dialogshowsavedialogsyncbrowserwindow-options"
   [data]
   (.then (.showSaveDialog dialog ^js @main-window (clj->js dialog-options))
-         (fn [^js/Promise file] (when-not (.-canceled file) (.writeFileSync fs (.-filePath file) data "utf-8")))))
+         (fn [^js/Promise file]
+           (when-not (.-canceled file)
+             (.writeFileSync fs (.-filePath file) data "utf-8")))))
+
+(defn open-to-renderer
+  [_err data]
+  (let [web-contents (.-webContents ^js @main-window)]
+    (.send web-contents "fromMain" (clj->js {:action "openDocument" :data data}))))
 
 (defn open
   "Opens a file.
    SEE https://www.electronjs.org/docs/api/dialog#dialogshowopendialogsyncbrowserwindow-options"
   []
   (.then (.showOpenDialog dialog ^js @main-window (clj->js dialog-options))
-         (fn [^js/Promise file] (when-not (.-canceled file)
-                                  (.readFileSync fs (.-filePath file) "utf-8" (fn [_err data]
-                                                                                (.send (.-webContents ^js @main-window) "fromMain" (clj->js  {:action "openDocument" :data data}))))))))
+         (fn [^js/Promise file]
+           (when-not (.-canceled file)
+             (.readFileSync fs (.-filePath file) "utf-8" open-to-renderer)))))
 
-(def export-options {:defaultPath default-path
-                     :filters [{:name "svg"
-                                :extensions ["svg" "svgo"]}
-                               {:name "png"
-                                :extensions ["png" "svgo"]}
-                               {:name "jpg"
-                                :extensions ["jpg" "jpeg"]}]})
+(def export-options
+  {:defaultPath default-path
+   :filters [{:name "svg"
+              :extensions ["svg" "svgo"]}
+             {:name "png"
+              :extensions ["png" "svgo"]}
+             {:name "jpg"
+              :extensions ["jpg" "jpeg"]}]})
 
 (defn export
   "Exports the provided data."
   [data]
   (.then (.showSaveDialog dialog ^js @main-window (clj->js export-options))
-         (fn [^js/Promise file] (when-not (.-canceled file)
-                                  (.writeFileSync fs (.-filePath file) data "utf-8")))))
+         (fn [^js/Promise file]
+           (when-not (.-canceled file)
+             (.writeFileSync fs (.-filePath file) data "utf-8")))))
