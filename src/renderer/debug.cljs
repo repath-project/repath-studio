560b9d22ff8fff064ca1bc0ecefbb909e75ed90b
Konstantinos Kaloutas <sprocketc@gmail.com>
2023-11-20 12:37:22 +0200

    fix keys

--- src/renderer/debug.cljs
+++ src/renderer/debug.cljs
@@ -12,40 +12,40 @@
 
 (defn info
   []
-  [:div.absolute.top-1.left-2.pointer-events-none
-   {:style {:color "#555"}}
-   (map (fn [[label value]] [:div [:strong.mr-1 label] value])
-        [["Content rect"
-          @(rf/subscribe [:content-rect])]
-         ["Viewbox"
-           (str (mapv units/->fixed @(rf/subscribe [:frame/viewbox])))]
+  (into [:div.absolute.top-1.left-2.pointer-events-none
+         {:style {:color "#555"}}]
+        (map (fn [[label value]] [:div [:strong.mr-1 label] value])
+             [["Content rect"
+               @(rf/subscribe [:content-rect])]
+              ["Viewbox"
+               (str (mapv units/->fixed @(rf/subscribe [:frame/viewbox])))]
 
-         ["Mouse position"
-          (str @(rf/subscribe [:mouse-pos]))]
+              ["Mouse position"
+               (str @(rf/subscribe [:mouse-pos]))]
 
-         ["Adjusted mouse position"
-          (str (mapv units/->fixed  @(rf/subscribe [:adjusted-mouse-pos])))]
+              ["Adjusted mouse position"
+               (str (mapv units/->fixed  @(rf/subscribe [:adjusted-mouse-pos])))]
 
-         ["Mouse offset"
-          (str @(rf/subscribe [:mouse-offset]))]
+              ["Mouse offset"
+               (str @(rf/subscribe [:mouse-offset]))]
 
-         ["Adjusted mouse offset"
-          (str (mapv units/->fixed  @(rf/subscribe [:adjusted-mouse-offset])))]
+              ["Adjusted mouse offset"
+               (str (mapv units/->fixed  @(rf/subscribe [:adjusted-mouse-offset])))]
 
-         ["Mouse drag?"
-          (str @(rf/subscribe [:drag?]))]
+              ["Mouse drag?"
+               (str @(rf/subscribe [:drag?]))]
 
-         ["Pan"
-          (str (mapv units/->fixed @(rf/subscribe [:document/pan])))]
+              ["Pan"
+               (str (mapv units/->fixed @(rf/subscribe [:document/pan])))]
 
-         ["Active tool"
-          @(rf/subscribe [:tool])]
+              ["Active tool"
+               @(rf/subscribe [:tool])]
 
-         ["Primary tool"
-          @(rf/subscribe [:primary-tool])]
+              ["Primary tool"
+               @(rf/subscribe [:primary-tool])]
 
-         ["State"
-          @(rf/subscribe [:state])]
+              ["State"
+               @(rf/subscribe [:state])]
 
-         ["Clicked element"
-          (:key @(rf/subscribe [:clicked-element]))]])])
+              ["Clicked element"
+               (:key @(rf/subscribe [:clicked-element]))]])))
