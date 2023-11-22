98ad49bd32f3a7576972bad80832c10cffbbffd9
Konstantinos Kaloutas <sprocketc@gmail.com>
2023-11-22 14:23:18 +0200

    add dnd docstrings

--- src/renderer/handlers.cljs
+++ src/renderer/handlers.cljs
@@ -14,6 +14,7 @@
   (assoc db :message message))
 
 (defn drop-files
+  "https://developer.mozilla.org/en-US/docs/Web/API/DataTransfer/files"
   [{:keys [adjusted-mouse-pos] :as db} files]
   (reduce
    (fn [db file]
@@ -24,26 +25,26 @@
                                     :attrs {:x (first adjusted-mouse-pos)
                                             :y (second adjusted-mouse-pos)
                                             :href (.-path file)}})
+
        db))
    db
    files))
 
 (defn drop-items
+  "https://developer.mozilla.org/en-US/docs/Web/API/DataTransferItem"
   [{:keys [adjusted-mouse-pos active-document] :as db} items]
   (let [fill (get-in db [:documents active-document :fill])]
     (doall
      (for [item items]
        (case (.-kind item)
-         "string"
-         (.getAsString
-          item
-          #(rf/dispatch [:element/create
-                         {:type :element
-                          :tag :text
-                          :content %
-                          :attrs {:x (first adjusted-mouse-pos)
-                                  :y (second adjusted-mouse-pos)
-                                  :fill fill}}]))
-
+         "string" (.getAsString
+                   item
+                   #(rf/dispatch [:element/create
+                                  {:type :element
+                                   :tag :text
+                                   :content %
+                                   :attrs {:x (first adjusted-mouse-pos)
+                                           :y (second adjusted-mouse-pos)
+                                           :fill fill}}]))
          nil))))
   db)
