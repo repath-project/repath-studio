9f0c177c39252d4fa95f97ed90847168fd333875
Konstantinos Kaloutas <sprocketc@gmail.com>
2023-12-21 10:19:50 +0200

    add attr descriptions

--- src/renderer/attribute/hierarchy.cljs
+++ src/renderer/attribute/hierarchy.cljs
@@ -139,3 +139,94 @@
   []
   "The style attribute allows to style an element using CSS declarations. 
    It functions identically to the style attribute in HTML.")
+
+
+(defmethod description :href
+  []
+  "The href attribute defines a link to a resource as a reference URL. 
+   The exact meaning of that link depends on the context of each element using it.")
+
+
+(defmethod description :attributeName
+  []
+  "The attributeName attribute indicates the name of the CSS property or 
+   attribute of the target element that is going to be changed during an animation.")
+
+(defmethod description :begin
+  []
+  "The begin attribute defines when an animation should begin.")
+
+(defmethod description :end
+  []
+  "The end attribute defines an end value for the animation that can constrain 
+   the active duration.")
+
+(defmethod description :dur
+  []
+  "The dur attribute indicates the simple duration of an animation.")
+
+(defmethod description :min
+  []
+  "The min attribute specifies the minimum value of the active animation duration.")
+
+(defmethod description :max
+  []
+  "The max attribute specifies the maximum value of the active animation duration.")
+
+(defmethod description :restart
+  []
+  "The restart attribute specifies whether or not an animation can restart.")
+
+(defmethod description :repeatCount
+  []
+  "The repeatCount attribute indicates the number of times an animation 
+   will take place.")
+
+(defmethod description :repeatDur
+  []
+  "The repeatDur attribute specifies the total duration for repeating an animation.")
+
+(defmethod description :calcMode
+  []
+  "The calcMode attribute specifies the interpolation mode for the animation.")
+
+(defmethod description :values
+  []
+  "The values attribute has different meanings, depending upon the context where it's used, 
+   either it defines a sequence of values used over the course of an animation, 
+   or it's a list of numbers for a color matrix, which is interpreted differently 
+   depending on the type of color change to be performed.")
+
+(defmethod description :keyTimes
+  []
+  "The keyTimes attribute represents a list of time values used to control 
+   the pacing of the animation.")
+
+(defmethod description :keySplines
+  []
+  "The keySplines attribute defines a set of Bézier curve control points 
+   associated with the keyTimes list, defining a cubic Bézier function 
+   that controls interval pacing")
+
+(defmethod description :from
+  []
+  "The from attribute indicates the initial value of the attribute that will be 
+   modified during the animation.")
+
+(defmethod description :to
+  []
+  "The to attribute indicates the final value of the attribute that will be 
+   modified during the animation.")
+
+(defmethod description :by
+  []
+  "The by attribute specifies a relative offset value for an attribute that will 
+   be modified during an animation.")
+
+(defmethod description :additive
+  []
+  "The additive attribute controls whether or not an animation is additive.")
+
+(defmethod description :accumulate
+  []
+  "The accumulate attribute controls whether or not an animation is cumulative.")
