5d0ee0ee72c47006eb7f1f8a86db7a19e3f3b7d1
Konstantinos Kaloutas <sprocketc@gmail.com>
2024-02-02 19:09:46 +0200

    remove fill from dropped strings

--- src/renderer/handlers.cljs
+++ src/renderer/handlers.cljs
@@ -32,19 +32,17 @@
 
 (defn drop-items
   "https://developer.mozilla.org/en-US/docs/Web/API/DataTransferItem"
-  [{:keys [adjusted-pointer-pos active-document] :as db} items]
-  (let [fill (get-in db [:documents active-document :fill])]
-    (doall
-     (for [item items]
-       (case (.-kind item)
-         "string" (.getAsString
-                   item
-                   #(rf/dispatch [:element/add
-                                  {:type :element
-                                   :tag :text
-                                   :content %
-                                   :attrs {:x (first adjusted-pointer-pos)
-                                           :y (second adjusted-pointer-pos)
-                                           :fill fill}}]))
-         nil))))
+  [{:keys [adjusted-pointer-pos] :as db} items]
+  (doall
+   (for [item items]
+     (case (.-kind item)
+       "string" (.getAsString
+                 item
+                 #(rf/dispatch [:element/add
+                                {:type :element
+                                 :tag :text
+                                 :content %
+                                 :attrs {:x (first adjusted-pointer-pos)
+                                         :y (second adjusted-pointer-pos)}}]))
+       nil)))
   db)
