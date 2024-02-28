dd3e5205e7db7300fd4e89dadcbcf3d5352c3931
Konstantinos Kaloutas <sprocketc@gmail.com>
2024-02-28 10:00:14 +0200

    enable js repl mode and refactor

--- src/renderer/reepl/core.cljs
+++ src/renderer/reepl/core.cljs
@@ -1,17 +1,16 @@
 (ns renderer.reepl.core
   (:require
-   ["react" :as react]
    ["react-resizable-panels" :refer [Panel PanelResizeHandle]]
    [cljs.reader]
    [cljs.tools.reader]
    [re-frame.core :as rf]
-   [reagent.core :as r]
+   [reagent.core :as ra]
    [renderer.components :as comp]
    [renderer.reepl.codemirror :as code-mirror]
    [renderer.reepl.completions :refer [completion-list]]
-   [renderer.reepl.handlers :as handlers]
+   [renderer.reepl.handlers :as h]
    [renderer.reepl.repl-items :refer [repl-items]]
-   [renderer.reepl.subs :as subs]
+   [renderer.reepl.subs :as s]
    [replumb.core :as replumb])
   (:require-macros
    [reagent.ratom :refer [reaction]]))
@@ -59,14 +58,15 @@
            (and in-place
                 (is-valid-cljs? source))))))})
 
-#_(defn repl-mode-button
-    [mode]
-    (let [repl-mode @(rf/subscribe [:repl-mode])
-          active? (= repl-mode mode)]
-      [:button.icon-button {:style {:class (when active? "overlay")
-                                    :color (when active? "var(--font-color-active)")}
-                            :on-click #(rf/dispatch [:set-repl-mode mode])}
-       mode]))
+(defn repl-mode-button
+  [mode]
+  (let [repl-mode @(rf/subscribe [:repl-mode])
+        active? (= repl-mode mode)]
+    [:button.button.p-1.rounded.m-1.uppercase.leading-none.text-xs
+     {:class (when active? "selected")
+      :style {:height "22px"}
+      :on-click #(rf/dispatch [:set-repl-mode mode])}
+     mode]))
 
 (defn repl-input
   [state submit cm-opts]
@@ -79,7 +79,7 @@
                        :on-change]))]}
   (let [{:keys [_pos _count _text]} @state
         repl-history? @(rf/subscribe [:panel/visible? :repl-history])]
-    [:div.toolbar {:style {:padding-top "0" :padding-bottom "0"}}
+    [:div.flex.pl-1.5.pr-2
      [:div.flex.text-xs.pl-1 {:style {:margin-top "7px"}} (replumb/get-prompt)]
      ^{:key (str (hash (:js-cm-opts cm-opts)))}
      [code-mirror/code-mirror (reaction (:text @state))
@@ -90,9 +90,9 @@
                 :padding "2px 0"}
         :on-eval submit}
        cm-opts)]
-     #_[:div
-        (repl-mode-button :cljs)
-        (repl-mode-button :js)]
+     [:div
+      (repl-mode-button :cljs)
+      (repl-mode-button :js)]
      [comp/toggle-icon-button
       {:class "hover:bg-transparent my-0"
        :active? repl-history?
@@ -101,7 +101,7 @@
        :inactive-icon "chevron-up"
        :inactive-text "Show command output"
        :action #(rf/dispatch [:panel/toggle :repl-history])}
-      {:style {:margin-top "0" :margin-bottom "0"}}]]))
+      {:style {:height "30px"}}]]))
 
 (defn set-print!
   [log]
@@ -125,87 +125,81 @@
 ;; TODO: is there a macro or something that could do this cleaner?
 (defn make-handlers
   [state]
-  {:add-input (partial swap! state handlers/add-input)
-   :add-result (partial swap! state handlers/add-result)
-   :go-up (partial swap! state handlers/go-up)
-   :go-down (partial swap! state handlers/go-down)
-   :clear-items (partial swap! state handlers/clear-items)
-   :set-text (partial swap! state handlers/set-text)
-   :add-log (partial swap! state handlers/add-log)})
+  {:add-input (partial swap! state h/add-input)
+   :add-result (partial swap! state h/add-result)
+   :go-up (partial swap! state h/go-up)
+   :go-down (partial swap! state h/go-down)
+   :clear-items (partial swap! state h/clear-items)
+   :set-text (partial swap! state h/set-text)
+   :add-log (partial swap! state h/add-log)})
+
+(defn repl-items-panel
+  [items show-value-opts set-text]
+  [:<>
+   [:> PanelResizeHandle
+    {:id "repl-resize-handle"
+     :className "resize-handle"}]
+   [:> Panel
+    {:id "repl-panel"
+     :minSize 10
+     :defaultSize 10
+     :order 3}
+    [repl-items items (assoc show-value-opts :set-text set-text)]]])
 
 (defn repl
   [& {:keys [execute
-             _complete-word
+             complete-word
              get-docs
              state
-             _show-value-opts
-             _js-cm-opts
-             _on-cm-init]}]
-  (let [state (or state (r/atom initial-state))
-        {:keys
-         [add-input
-          add-result
-          go-up
-          go-down
-          clear-items
-          set-text
-          add-log]} (make-handlers state)
-
-        items (subs/items state)
-        complete-atom (r/atom nil)
-        docs (reaction
-              (let [{:keys [pos list] :as state} @complete-atom]
-                (when state
-                  (let [sym (first (get list pos))]
-                    (when (symbol? sym)
-                      (get-docs sym))))))
-        submit (fn [text]
-                 (if (= "clear" (.trim text))
-                   (do
-                     (clear-items)
-                     (set-text ""))
-                   (when (pos? (count (.trim text)))
-                     (set-text text)
-                     (add-input text)
-                     (execute text #(add-result (not %1) %2)))))]
+             show-value-opts
+             js-cm-opts
+             on-cm-init]}]
+  (ra/with-let [state (or state (ra/atom initial-state))
+                {:keys [add-input
+                        add-result
+                        go-up
+                        go-down
+                        clear-items
+                        set-text
+                        add-log]} (make-handlers state)
+                items (s/items state)
+                complete-atom (ra/atom nil)
+                docs (reaction
+                      (let [{:keys [pos list] :as state} @complete-atom]
+                        (when state
+                          (let [sym (first (get list pos))]
+                            (when (symbol? sym)
+                              (get-docs sym))))))
+                submit (fn [text]
+                         (if (= "clear" (.trim text))
+                           (do
+                             (clear-items)
+                             (set-text ""))
+                           (when (pos? (count (.trim text)))
+                             (set-text text)
+                             (add-input text)
+                             (execute text #(add-result (not %1) %2)))))]
 
     (set-print! add-log)
+    [:<>
+     (when @(rf/subscribe [:panel/visible? :repl-history])
+       [repl-items-panel @items show-value-opts set-text])
 
-    (fn [& {:keys [_execute
-                   complete-word
-                   _get-docs
-                   state
-                   show-value-opts
-                   js-cm-opts
-                   on-cm-init]}]
-      [:<>
-       (when @(rf/subscribe [:panel/visible? :repl-history])
-         [:<>
-          [:> PanelResizeHandle
-           {:id "repl-resize-handle"
-            :className "resize-handle"}]
-          [:> Panel
-           {:id "repl-panel"
-            :minSize 10
-            :defaultSize 10
-            :order 3}
-           [repl-items @items (assoc show-value-opts :set-text set-text)]]])
-
-       [:div.relative.whitespace-pre-wrap.font-mono
-        [completion-list
-         @docs
-         @complete-atom
+     [:div.relative.whitespace-pre-wrap.font-mono
+      [completion-list
+       @docs
+       @complete-atom
         ;; TODO: this should also replace the text....
-         identity
-         #_(swap! complete-atom assoc :pos % :active? true)]
-        (let [_items @items] ; TODO: This needs to be removed
-          [repl-input
-           (subs/current-text state)
-           submit
-           {:complete-word complete-word
-            :on-up go-up
-            :on-down go-down
-            :complete-atom complete-atom
-            :on-change set-text
-            :js-cm-opts js-cm-opts
-            :on-cm-init on-cm-init}])]])))
+       identity
+       #_(swap! complete-atom assoc :pos % :active? true)]
+      (let [_items @items] ; TODO: This needs to be removed
+        [repl-input
+         (s/current-text state)
+         submit
+         {:complete-word complete-word
+          :on-up go-up
+          :on-down go-down
+          :complete-atom complete-atom
+          :on-change set-text
+          :js-cm-opts js-cm-opts
+          :on-cm-init on-cm-init}])]]))
