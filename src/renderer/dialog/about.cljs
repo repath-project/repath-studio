076013b925ec9e886454e334aae82bf77f61c1ee
Konstantinos Kaloutas <sprocketc@gmail.com>
2024-03-14 14:25:00 +0200

    fix about dialog

--- src/renderer/dialog/about.cljs
+++ src/renderer/dialog/about.cljs
@@ -1,14 +1,21 @@
 (ns renderer.dialog.about
   (:require
    [config]
+   [platform]
+   [re-frame.core :as rf]
    [renderer.dialog.views :as v]))
 
 (defn root
   []
-  [:div.p-4.flex.gap-3
-   [:img.w-24 {:src "/img/icon.svg"}]
-   [:div
-    [:h1.text-3xl.mb-2 "Repath Studio"]
-    [:div (str "Version: " config/version)]
-    [:div (str "Platform: " js/window.api.platform)]]
+  [:div.p-4
+   [:div.flex.gap-3.items-start.pb-2
+    [:img.w-24 {:src "/img/icon.svg"}]
+    [:div
+     [:h1.text-3xl.mb-2.font-light "Repath Studio"]
+     [:div  [:strong "Version: "] config/version]
+     [:div  [:strong "Browser: "] platform/user-agent]]]
+   [:div.flex.justify-end
+    [:button.button.px-2.bg-primary.rounded
+     {:on-click #(rf/dispatch [:dialog/close])}
+     "OK"]]
    [v/close]])
