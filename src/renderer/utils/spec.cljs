ccfaa67a3a65949875ab33827944f6cdd4d1b907
Konstantinos Kaloutas <sprocketc@gmail.com>
2024-05-26 12:47:08 +0300

    upgrade mdn-bcd

--- src/renderer/utils/spec.cljs
+++ src/renderer/utils/spec.cljs
@@ -6,21 +6,28 @@
 (def svg
   (js->clj (.-svg bcd) :keywordize-keys true))
 
+(def core-attrs
+  #{:id :class :style})
+
 (def presentation-attrs
-  "TODO: Removed on @mdn/browser-compat-data 5.5.14.
-   https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/Presentation"
-  (-> svg
-      :attributes
-      :presentation
-      keys
-      set))
+  #{:text-anchor :text-rendering :font-style :mask :image-rendering
+    :stroke-dasharray :fill-rule :font-stretch :text-overflow :vector-effect
+    :stroke :stop-color :clip :glyph-orientation-horizontal :solid-opacity
+    :transform :color :white-space :font-size :kerning :font-variant
+    :writing-mode :font-weight :overflow :clip-rule :stroke-opacity :fill
+    :color-profile :stroke-linejoin :shape-rendering :cursor :stroke-dashoffset
+    :word-spacing :clip-path :stroke-linecap :flood-opacity :lighting-color
+    :alignment-baseline :dominant-baseline :marker-start :filter :stroke-width
+    :opacity :baseline-shift :color-interpolation-filters :transform-origin
+    :text-decoration :display :stroke-miterlimit :letter-spacing :flood-color
+    :unicode-bidi :marker-mid :pointer-events :font-size-adjust
+    :glyph-orientation-vertical :color-interpolation :visibility
+    :enable-background :direction :fill-opacity :solid-color :font-family
+    :marker-end :paint-order})
 
 (defn compat-data
   "Returns conmpatibility data for tags or attributes."
   ([tag]
    (-> svg :elements tag :__compat))
   ([tag attr]
-   (or (-> svg :elements tag attr :__compat)
-       (-> svg :attributes :presentation attr :__compat)
-       (-> svg :attributes :core attr :__compat)
-       (-> svg :attributes :style attr :__compat))))
+   (-> svg :elements tag attr :__compat)))
