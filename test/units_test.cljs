75fcb0e869f48c712a2225cf186a3ab3f3ae019d
Konstantinos Kaloutas <sprocketc@gmail.com>
2024-08-01 13:38:21 +0300

    enhance tests

--- test/units_test.cljs
+++ test/units_test.cljs
@@ -1,35 +1,39 @@
 (ns units-test
   (:require
-   [cljs.test :refer-macros [deftest testing is]]
+   [cljs.test :refer-macros [deftest testing are]]
    [renderer.utils.units :as units]))
 
 (deftest test-unit->key
   (testing "convert unit string to keyword"
-    (is (= (units/unit->key "px") :px))
-    (is (= (units/unit->key "Px") :px))))
+    (are [x y] (= x y)
+      :px (units/unit->key "px")
+      :px (units/unit->key "Px"))))
 
 (deftest test-valid-unit?
   (testing "check if unit is valid"
-    (is (= (units/valid-unit? "px") true))
-    (is (= (units/valid-unit? "em") true))
-    (is (= (units/valid-unit? "rem") true))
-    (is (= (units/valid-unit? "foo") false))
-    (is (= (units/valid-unit? "") false))))
+    (are [x y] (= x y)
+      true (units/valid-unit? "px")
+      true (units/valid-unit? "em")
+      true (units/valid-unit? "rem")
+      false (units/valid-unit? "foo")
+      false (units/valid-unit? ""))))
 
 (deftest test-match-unit
   (testing "match unit"
-    (is (= (units/match-unit "5px") "px"))
-    ;; TODO: The following case should not work. We need to adjust the regex.
-    (is (= (units/match-unit "5 px") "px"))
-    (is (= (units/match-unit "5454px") "px"))
-    (is (= (units/match-unit "px") "px"))
-    (is (= (units/match-unit "0") ""))))
+    (are [x y] (= x y)
+      "px" (units/match-unit "5px")
+          ;; TODO: The following case should not work. We need to adjust the regex.
+      "px" (units/match-unit "5 px")
+      "px" (units/match-unit "5454px")
+      "px" (units/match-unit "px")
+      "" (units/match-unit "0"))))
 
 (deftest test-parse-unit
   (testing "parse unit"
-    (is (= (units/parse-unit "5px") [5 "px"]))
-    (is (= (units/parse-unit " 5px ") [5 "px"]))
-    (is (= (units/parse-unit " 5 px ") [5 "px"]))
-    (is (= (units/parse-unit "5454px") [5454 "px"]))
-    (is (= (units/parse-unit "px") [0 "px"]))
-    (is (= (units/parse-unit "0") [0 ""]))))
+    (are [x y] (= x y)
+      [5 "px"] (units/parse-unit "5px")
+      [5 "px"] (units/parse-unit " 5px ")
+      [5 "px"] (units/parse-unit " 5 px ")
+      [5454 "px"] (units/parse-unit "5454px")
+      [0 "px"] (units/parse-unit "px")
+      [0 ""] (units/parse-unit "0"))))
