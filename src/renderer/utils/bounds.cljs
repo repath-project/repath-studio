c74d77c20070c390dddd167f8b50e0b4a3b0c828
Konstantinos Kaloutas <sprocketc@gmail.com>
2024-08-25 10:49:16 +0300

    refactor bounds

--- src/renderer/utils/bounds.cljs
+++ src/renderer/utils/bounds.cljs
@@ -12,11 +12,11 @@
    https://developer.mozilla.org/en-US/docs/Web/API/SVGGraphicsElement/getBBox"
   [^js/Element el]
   (when (.-getBBox el)
-    (let [bounds (.getBBox el)
-          x1 (.-x bounds)
-          y1 (.-y bounds)
-          x2 (+ x1 (.-width bounds))
-          y2 (+ y1 (.-height bounds))]
+    (let [b (.getBBox el)
+          x1 (.-x b)
+          y1 (.-y b)
+          x2 (+ x1 (.-width b))
+          y2 (+ y1 (.-height b))]
       [x1 y1 x2 y2])))
 
 (mx/defn union :- bounds
@@ -32,33 +32,27 @@
 
 (mx/defn center :- math/point
   "Calculates the center of bounds."
-  [bounds :- bounds]
-  (mat/add (take 2 bounds)
-           (mat/div (->dimensions bounds) 2)))
+  [b :- bounds]
+  (mat/add (take 2 b)
+           (mat/div (->dimensions b) 2)))
 
 (mx/defn intersect? :- boolean?
   "Tests whether the provided set of bounds intersect."
-  [a-bounds :- bounds
-   b-bounds :- bounds]
-  (when (and a-bounds b-bounds)
-    (let [[a-left a-top a-right a-bottom] a-bounds
-          [b-left b-top b-right b-bottom] b-bounds]
-      (not (or (> b-left a-right)
-               (< b-right a-left)
-               (> b-top a-bottom)
-               (< b-bottom a-top))))))
+  [[a-left a-top a-right a-bottom] :- bounds
+   [b-left b-top b-right b-bottom] :- bounds]
+  (not (or (> b-left a-right)
+           (< b-right a-left)
+           (> b-top a-bottom)
+           (< b-bottom a-top))))
 
 (mx/defn contained? :- boolean?
   "Tests whether `bounds-a` fully contain `bounds-b`."
-  [a-bounds :- bounds
-   b-bounds :- bounds]
-  (when (and a-bounds b-bounds)
-    (let [[a-left a-top a-right a-bottom] a-bounds
-          [b-left b-top b-right b-bottom] b-bounds]
-      (and (> a-left b-left)
-           (> a-top b-top)
-           (< a-right b-right)
-           (< a-bottom b-bottom)))))
+  [[a-left a-top a-right a-bottom] :- bounds
+   [b-left b-top b-right b-bottom] :- bounds]
+  (and (> a-left b-left)
+       (> a-top b-top)
+       (< a-right b-right)
+       (< a-bottom b-bottom)))
 
 (mx/defn contain-point? :- boolean?
   "Tests whether the provided bounds contain a point."
