e1b3de2c889744b0a1f741e49065b95859b21865
Konstantinos Kaloutas <sprocketc@gmail.com>
2024-08-28 12:21:33 +0300

    fix dropper deactivation

--- src/renderer/tool/misc/dropper.cljs
+++ src/renderer/tool/misc/dropper.cljs
@@ -1,5 +1,6 @@
 (ns renderer.tool.misc.dropper
   (:require
+   [re-frame.core :as rf]
    [renderer.app.events :as-alias app.e]
    [renderer.app.handlers :as app.h]
    [renderer.color.effects :as-alias color.fx]
@@ -22,8 +23,13 @@
   (if (.-EyeDropper js/window)
     (-> db
         (app.h/set-message "Click anywhere to pick a color.")
-        (app.h/add-fx [::color.fx/dropper {:on-success ::document.e/set-fill}])
-        (app.h/add-fx [:dispatch [::app.e/set-tool :select]]))
+        (app.h/add-fx [::color.fx/dropper {:on-success ::set-fill-and-deactivate}]))
     (-> db
         (notification.h/add [notification.v/unavailable-feature "EyeDropper" "https://developer.mozilla.org/en-US/docs/Web/API/EyeDropper_API#browser_compatibility"])
         (app.h/set-tool :select))))
+
+(rf/reg-event-fx
+ ::set-fill-and-deactivate
+ (fn [_ [_ color]]
+   {:fx [[:dispatch [::document.e/set-fill color]]
+         [:dispatch [::app.e/set-tool :select]]]}))
