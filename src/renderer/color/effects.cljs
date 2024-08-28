4a257120d7e4b5842278682b19eff82051a8f674
Konstantinos Kaloutas <sprocketc@gmail.com>
2024-08-28 12:12:59 +0300

    refactor dropper

--- src/renderer/color/effects.cljs
+++ src/renderer/color/effects.cljs
@@ -7,19 +7,11 @@
 (rf/reg-fx
  ::dropper
  (fn [{:keys [on-success]}]
-   (if (.-EyeDropper js/window)
-     (-> (js/EyeDropper.)
-         (.open)
-         (.then (fn [^js/Object color]
-                  (rf/dispatch [on-success (.-sRGBHex color)])
-                  (rf/dispatch [::app.e/set-tool :select])))
-         (.catch (fn [error]
-                    (rf/dispatch [::notification.e/add
-                                  [:div
-                                   [:h2.pb-4.font-bold "EyeDropper cannot be activated."]
-                                   [:div.text-error (str error)]]])
-                    (rf/dispatch [::app.e/set-tool :select]))))
-     (do (rf/dispatch [::notification.e/unavailable-feature
-                       "EyeDropper"
-                       "https://developer.mozilla.org/en-US/docs/Web/API/EyeDropper_API#browser_compatibility"])
-         (rf/dispatch [::app.e/set-tool :select])))))
+   (-> (js/EyeDropper.)
+       (.open)
+       (.then (fn [^js/Object color] (rf/dispatch [on-success (.-sRGBHex color)])))
+       (.catch (fn [error]
+                 (rf/dispatch [::notification.e/add
+                               [:div
+                                [:h2.pb-4.font-bold "EyeDropper cannot be activated."]
+                                [:div.text-error (str error)]]]))))))
--- src/renderer/tool/misc/dropper.cljs
+++ src/renderer/tool/misc/dropper.cljs
@@ -1,10 +1,13 @@
 (ns renderer.tool.misc.dropper
   (:require
+   [renderer.app.events :as-alias app.e]
    [renderer.app.handlers :as app.h]
    [renderer.color.effects :as-alias color.fx]
    [renderer.document.events :as-alias document.e]
    [renderer.element.events :as-alias element.e]
    [renderer.notification.events :as-alias notification.e]
+   [renderer.notification.handlers :as notification.h]
+   [renderer.notification.views :as notification.v]
    [renderer.tool.base :as tool]))
 
 (derive :dropper ::tool/tool)
@@ -16,6 +19,11 @@
 
 (defmethod tool/activate :dropper
   [db]
-  (-> db
-      (app.h/set-message "Click anywhere to pick a color.")
-      (app.h/add-fx [::color.fx/dropper {:on-success ::document.e/set-fill}])))
+  (if (.-EyeDropper js/window)
+    (-> db
+        (app.h/set-message "Click anywhere to pick a color.")
+        (app.h/add-fx [::color.fx/dropper {:on-success ::document.e/set-fill}])
+        (app.h/add-fx [:dispatch [::app.e/set-tool :select]]))
+    (-> db
+        (notification.h/add [notification.v/unavailable-feature "EyeDropper" "https://developer.mozilla.org/en-US/docs/Web/API/EyeDropper_API#browser_compatibility"])
+        (app.h/set-tool :select))))
