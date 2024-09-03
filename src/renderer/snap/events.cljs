26acfcb8cb0650c48230b11cda0d6e250103af7e
Konstantinos Kaloutas <sprocketc@gmail.com>
2024-09-03 12:20:49 +0300

    persist snap options

--- src/renderer/snap/events.cljs
+++ src/renderer/snap/events.cljs
@@ -1,14 +1,17 @@
 (ns renderer.snap.events
   (:require
-   [re-frame.core :as rf]))
+   [re-frame.core :as rf]
+   [renderer.app.events :refer [persist]]))
 
 (rf/reg-event-db
  ::toggle
+ [persist]
  (fn [db [_]]
    (update-in db [:snap :enabled?] not)))
 
 (rf/reg-event-db
  ::toggle-option
+ [persist]
  (fn [{:keys [snap] :as db} [_ option]]
    (if (contains? (:options snap) option)
      (update-in db [:snap :options] disj option)
