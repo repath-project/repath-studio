127ba41c0b07529f4503b84b1706810ab661dc0d
Konstantinos Kaloutas <sprocketc@gmail.com>
2024-09-25 11:17:13 +0300

    refactor migrations

--- src/renderer/utils/compatibility.cljs
+++ src/renderer/utils/compatibility.cljs
@@ -8,30 +8,37 @@
 (def ver-regex
   #"(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$")
 
-(def Version [:tuple int? int? int?])
+(def Version
+  [:tuple
+   [number? {:title "major"}]
+   [number? {:title "minor"}]
+   [number? {:title "patch"}]])
 
-(mx/defn version->table :- [:maybe Version]
-  [s :- [:maybe [:re ver-regex]]]
-  (->> s
-       (str)
-       (re-find ver-regex)
+(mx/defn version->vec :- Version
+  [s :- string?]
+  (->> (re-find ver-regex s)
        (rest)
        (take 3)
        (mapv js/parseInt)))
 
 (mx/defn requires-migration? :- boolean?
-  [document :- map?, [m-major m-minor m-patch] :- Version]
-  (let [[major minor patch] (version->table (:version document))]
-    (or (< major m-major)
-        (and (= major m-major)
-             (< minor m-minor))
-        (and (= major m-major)
-             (= minor m-minor)
-             (< patch m-patch)))))
+  [document-version :- Version, migration-version :- Version]
+  (let [[m-major m-minor m-patch] migration-version
+        [d-major d-minor d-patch] document-version]
+    (or (< d-major m-major)
+        (and (= d-major m-major)
+             (< d-minor m-minor))
+        (and (= d-major m-major)
+             (= d-minor m-minor)
+             (< d-patch m-patch)))))
 
 (mx/defn migrate-document :- PersistedDocument
   [document :- map?]
-  (reduce (fn [document [m-ver f]]
+  (reduce (fn [document [migration-version migration-f]]
             (cond-> document
-              (requires-migration? document m-ver) f)) document migrations/migrations))
+              (:or (not (:version document))
+                   (requires-migration? (version->vec (:version document)) migration-version))
+              migration-f))
+          document
+          migrations/migrations))
 
--- test/compatibility_test.cljs
+++ test/compatibility_test.cljs
@@ -3,16 +3,16 @@
    [cljs.test :refer-macros [deftest testing are]]
    [renderer.utils.compatibility :as compatibility]))
 
-(deftest test-version->table
-  (testing "version to table"
+(deftest test-version->vec
+  (testing "version to vector"
     (are [x y] (= x y)
-      [0 3 3] (compatibility/version->table "0.3.3-2-4cd3bf6-SNAPSHOT")
-      [123 0 3] (compatibility/version->table "123.0.3-32423423"))))
+      [0 3 3] (compatibility/version->vec "0.3.3-2-4cd3bf6-SNAPSHOT")
+      [123 0 3] (compatibility/version->vec "123.0.3-32423423"))))
 
 (deftest test-requires-migration?
   (testing "migration requirement"
     (are [x y] (= x y)
-      true (compatibility/requires-migration? {:version "0.3.3-2-4cd3bf6-SNAPSHOT"} [0 4 0])
-      true (compatibility/requires-migration? {:version "0.3.3-2-4cd3bf6-SNAPSHOT"} [0 3 4])
-      false (compatibility/requires-migration? {:version "1.3.3-2-4cd3bf6-SNAPSHOT"} [0 3 4])
-      false (compatibility/requires-migration? {:version "1.3.3-2-4cd3bf6-SNAPSHOT"} [1 3 3]))))
+      true (compatibility/requires-migration? [0 3 3] [0 4 0])
+      true (compatibility/requires-migration? [0 3 3] [0 3 4])
+      false (compatibility/requires-migration? [1 3 3] [0 3 4])
+      false (compatibility/requires-migration? [1 3 3] [1 3 3]))))
