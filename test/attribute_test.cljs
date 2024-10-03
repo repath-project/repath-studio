676415f0053d9c88171376e6a15a6ce8547ea289
Konstantinos Kaloutas <sprocketc@gmail.com>
2024-10-03 12:19:13 +0300

    add tests for attribute ns

---
+++ test/attribute_test.cljs
@@ -0,0 +1,55 @@
+(ns attribute-test
+  (:require
+   [cljs.test :refer-macros [deftest testing are]]
+   [renderer.utils.attribute :as attr]))
+
+(deftest test-str->seq
+  (testing "string to sequence conversion"
+    (are [x y] (= x y)
+      ["0" "1"] (attr/str->seq "0 1")
+      ["0" "12" "342.3" "4352345345" "454535" "34"] (attr/str->seq "0 12 342.3 4352345345 454535 34"))))
+
+(deftest test-points->vec
+  (testing "string to point vector conversion"
+    (are [x y] (= x y)
+      [["0" "12"] ["342.3" "4352345345"] ["454535" "34"]] (attr/points->vec "0 12 342.3 4352345345 454535 34")
+      [["0" "12"] ["342.3" "4352345345"] ["454535" "34"] ["1"]] (attr/points->vec "0 12 342.3 4352345345 454535 34 1"))))
+
+(deftest test-points->px
+  (testing "string to pixel point vector conversion"
+    (are [x y] (= x y)
+      [[0 12] [342.3 4352345345] [454535 34]] (attr/points->px "0 12 342.3 4352345345 454535 34")
+      [[0 12] [342.3 4352345345] [454535 34] [1]] (attr/points->px "0 12 342.3 4352345345 454535 34 1")
+      [[0 12] [342.3 4352345345] [454535 34] [1]] (attr/points->px "0px 12PX 342.3 4352345345 454535 34 1"))))
+
+(deftest test->camel-case
+  (testing "attribute key to camel-case conversion"
+    (are [x y] (= x y)
+      :viewBox (attr/->camel-case :viewbox)
+      :glyphOrientationHorizontal (attr/->camel-case :Glyphorientationhorizontal))))
+
+(deftest test->camel-case-memo
+  (testing "memoized attribute key to camel-case conversion"
+    (are [x y] (= x y)
+      :viewBox (attr/->camel-case :viewbox)
+      :glyphOrientationHorizontal (attr/->camel-case :Glyphorientationhorizontal))))
+
+
+(deftest test-defaults
+  (testing "default tag attributed"
+    (are [x y] (= x y)
+      {:x ""
+       :y ""
+       :rx ""
+       :ry ""
+       :width ""
+       :height ""
+       :fill ""
+       :stroke ""
+       :stroke-dasharray ""
+       :stroke-linejoin ""
+       :style ""
+       :stroke-width ""
+       :opacity ""
+       :id ""
+       :class ""} (attr/defaults :rect))))
