9ce67f21e016a32463b4d3585d51aa2b54db8674
Konstantinos Kaloutas <sprocketc@gmail.com>
2024-10-03 12:00:01 +0300

    add partial-right tests

---
+++ test/extra_test.cljs
@@ -0,0 +1,10 @@
+(ns extra-test
+  (:require
+   [cljs.test :refer-macros [deftest testing are]]
+   [renderer.utils.extra :refer [partial-right]]))
+
+(deftest test-partial-right
+  (testing "partial right"
+    (are [x y] (= x y)
+      [1 0 2 0 3 0] (reduce (partial-right conj 0) [] [1 2 3])
+      "1a2a3a" (reduce (partial-right str "a") "" [1 2 3]))))
