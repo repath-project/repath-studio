35e3beca9a25cd83d32a60cfc300dc70d2b29440
Konstantinos Kaloutas <sprocketc@gmail.com>
2024-10-16 19:47:22 +0300

    add tests

--- test/app_test.cljs
+++ test/app_test.cljs
@@ -1,6 +1,6 @@
 (ns app-test
   (:require
-   [cljs.test :refer-macros [deftest are]]
+   [cljs.test :refer-macros [deftest is are]]
    [day8.re-frame.test :as rf-test]
    [re-frame.core :as rf]
    [renderer.app.events :as e]
@@ -14,3 +14,40 @@
      :select @(rf/subscribe [::s/tool])
      {} @(rf/subscribe [::s/documents])
      [] @(rf/subscribe [::s/document-tabs]))))
+
+(deftest tool
+  (rf-test/run-test-sync
+   (rf/dispatch [::e/initialize-db])
+
+   (let [active-tool (rf/subscribe [::s/tool])]
+     (is (= @active-tool :select))
+
+     (rf/dispatch [::e/set-tool :rect])
+     (is (= @active-tool :rect)))))
+
+(deftest lang
+  (rf-test/run-test-sync
+   (rf/dispatch [::e/initialize-db])
+   (rf/dispatch [::e/set-lang :en-US])
+   (is (= :en-US @(rf/subscribe [::s/lang])))))
+
+(deftest grid
+  (rf-test/run-test-sync
+   (rf/dispatch [::e/initialize-db])
+
+   (let [grid-visible (rf/subscribe [::s/grid])]
+     (is (not @grid-visible))
+
+     (rf/dispatch [::e/toggle-grid])
+     (is @grid-visible))))
+
+
+(deftest panel
+  (rf-test/run-test-sync
+   (rf/dispatch [::e/initialize-db])
+
+   (let [tree-visible (rf/subscribe [::s/panel-visible :tree])]
+     (is @tree-visible)
+
+     (rf/dispatch [::e/toggle-panel :tree])
+     (is (not @tree-visible)))))
