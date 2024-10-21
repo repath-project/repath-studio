337dabba128fd95d85e389dd60fffe7dab16e904
Konstantinos Kaloutas <sprocketc@gmail.com>
2024-10-21 12:41:05 +0300

    add tests

--- test/element_impl_test.cljs
+++ test/element_impl_test.cljs
@@ -151,3 +151,117 @@
 
     (is (= (hierarchy/path line-el)
            "M 0 0 L 50 50"))))
+
+(deftest polygon
+  (let [polygon-el {:type :element
+                    :tag :polygon
+                    :attrs {:points "528 -305 718 -370 941 -208"}}]
+
+    (is (= (hierarchy/translate polygon-el [10 10])
+           {:type :element
+            :tag :polygon
+            :attrs {:points "538 -295 728 -360 951 -198"}}))
+
+    (is (= (hierarchy/scale polygon-el [2 2] [25 25])
+           {:type :element
+            :tag :polygon
+            :attrs {:points "503 -265 883 -395 1329 -71"}}))
+
+    (is (= (hierarchy/scale polygon-el [2 2] [0 0])
+           {:type :element
+            :tag :polygon
+            :attrs {:points "528 -240 908 -370 1354 -46"}}))
+
+    (is (= (hierarchy/bounds polygon-el)
+           [528 -370 941 -208]))
+
+    (is (= (hierarchy/path polygon-el)
+           "M528 -305 718 -370 941 -208z"))))
+
+(deftest polyline
+  (let [polyline-el {:type :element
+                     :tag :polyline
+                     :attrs {:points "528 -305 718 -370 941 -208"}}]
+
+    (is (= (hierarchy/translate polyline-el [10 10])
+           {:type :element
+            :tag :polyline
+            :attrs {:points "538 -295 728 -360 951 -198"}}))
+
+    (is (= (hierarchy/scale polyline-el [2 2] [25 25])
+           {:type :element
+            :tag :polyline
+            :attrs {:points "503 -265 883 -395 1329 -71"}}))
+
+    (is (= (hierarchy/scale polyline-el [2 2] [0 0])
+           {:type :element
+            :tag :polyline
+            :attrs {:points "528 -240 908 -370 1354 -46"}}))
+
+    (is (= (hierarchy/bounds polyline-el)
+           [528 -370 941 -208]))
+
+    (is (= (hierarchy/path polyline-el)
+           "M528 -305 718 -370 941 -208"))))
+
+(deftest path
+  (let [path-el {:type :element
+                 :tag :path
+                 :attrs {:d "M528 -305 718 -371 941 -208 663 -174 664 -261z"}}]
+
+    (is (= (hierarchy/translate path-el [10 10])
+           {:type :element
+            :tag :path
+            :attrs {:d "M538-295L728-361 951-198 673-164 674-251z"}}))
+
+    (is (= (hierarchy/scale path-el [2 2] [25 25])
+           {:type :element
+            :tag :path
+            :attrs {:d "M503-264L883-396 1329-70 773-2 775-176z"}}))
+
+    (is (= (hierarchy/scale path-el [2 2] [0 0])
+           {:type :element
+            :tag :path
+            :attrs {:d "M528-239L908-371 1354-45 798 23 800-151z"}}))
+
+    (is (= (hierarchy/bounds path-el)
+           [528 -371 941 -174]))
+
+    (is (thrown? js/Error (hierarchy/path path-el)))))
+
+(deftest svg
+  (let [svg-el {:type :element
+                :tag :svg
+                :attrs {:x "0"
+                        :y "0"
+                        :width "50"
+                        :height "50"}}]
+
+    (is (= (hierarchy/translate svg-el [50 50])
+           {:type :element
+            :tag :svg
+            :attrs {:x "50"
+                    :y "50"
+                    :width "50"
+                    :height "50"}}))
+
+    (is (= (hierarchy/scale svg-el [2 2] [25 25])
+           {:type :element
+            :tag :svg
+            :attrs {:x "-25"
+                    :y "-25"
+                    :width "100"
+                    :height "100"}}))
+
+    (is (= (hierarchy/scale svg-el [2 2] [0 0])
+           {:type :element
+            :tag :svg
+            :attrs {:x "0"
+                    :y "0"
+                    :width "100"
+                    :height "100"}}))
+
+    (is (= (hierarchy/bounds svg-el)
+           [0 0 50 50]))
+
+    (is (thrown? js/Error (hierarchy/path svg-el)))))
