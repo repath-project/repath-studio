a9fc9830ac01b7dc15855b58cf8b029c5ac648a4
Konstantinos Kaloutas <sprocketc@gmail.com>
2024-11-14 18:50:00 +0200

    fix recenter-to-dom-rect

--- src/renderer/frame/handlers.cljs
+++ src/renderer/frame/handlers.cljs
@@ -43,8 +43,8 @@
                    (select-keys [:width :height]))]
     (if-not (-> db :window :focused)
       db
-      (-> (:document-tabs db)
-          (reduce #(pan-by %1 (mat/div [(:width offset) (:height offset)] 2) %2) db)))))
+      (->> (:document-tabs db)
+           (reduce #(pan-by %1 (mat/div [(:width offset) (:height offset)] 2) %2) db)))))
 
 (m/=> zoom-in-place [:-> App number? Vec2D App])
 (defn zoom-in-place
