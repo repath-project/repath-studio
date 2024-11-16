905a2bf55f51eaff7d49976f28d4b6c268854afc
Konstantinos Kaloutas <sprocketc@gmail.com>
2024-11-16 09:59:29 +0200

    fix typo

--- src/renderer/theme/db.cljs
+++ src/renderer/theme/db.cljs
@@ -1,7 +1,7 @@
 (ns renderer.theme.db)
 
 ;; The iframe is isolated so we don't have access to the css vars of the parent.
-;; We are currently using hardcoded values, but we hould be able to set those
+;; We are currently using hardcoded values, but we should be able to set those
 ;; vars in the nested document if we have to.
 (def accent-inverted "#fff")
 (def accent "#e93976")
