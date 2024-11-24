e9f3b21885948d9f067443984556a59c9244cd7b
Konstantinos Kaloutas <sprocketc@gmail.com>
2024-11-24 20:27:03 +0200

    Revert "remove DomElement schema"
    This reverts commit aeca4dd5133e2f86a2180edec168c458e4723e7e.

--- src/renderer/utils/bounds.cljs
+++ src/renderer/utils/bounds.cljs
@@ -3,6 +3,7 @@
    [clojure.core.matrix :as mat]
    [malli.core :as m]
    [renderer.snap.db :refer [SnapOptions]]
+   [renderer.utils.dom :refer [DomElement]]
    [renderer.utils.math :refer [Vec2D]]))
 
 (def Bounds
@@ -13,7 +14,7 @@
    [number? {:title "right"}]
    [number? {:title "bottom"}]])
 
-(m/=> dom-el->bounds [:-> any? [:maybe Bounds]])
+(m/=> dom-el->bounds [:-> DomElement [:maybe Bounds]])
 (defn dom-el->bounds
   "Experimental way of getting the bounds of unknown or complicated elements
    using the getBBox method.
--- src/renderer/utils/dom.cljs
+++ src/renderer/utils/dom.cljs
@@ -1,5 +1,8 @@
 (ns renderer.utils.dom)
 
+(def DomElement
+  [:fn (fn [x] (instance? js/Element x))])
+
 (defn prevent-default!
   [e]
   (.preventDefault e))
