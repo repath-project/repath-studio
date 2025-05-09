d60e8efa7a8e6f9c17c09ed0eea0b4f1441d9aa7
Konstantinos Kaloutas <sprocketc@gmail.com>
2025-05-09 15:31:03 +0300

    fix accent color

--- src/renderer/theme/db.cljs
+++ src/renderer/theme/db.cljs
@@ -4,7 +4,7 @@
 ;; We are currently using hardcoded values, but we should be able to set those
 ;; vars in the nested document if we have to.
 (def accent-inverted "#fff")
-(def accent "#e93976")
+(def accent "#d12b65")
 (def font-mono "'Consolas (Custom)', 'Bitstream Vera Sans Mono', monospace, 'Apple Color Emoji', 'Segoe UI Emoji'")
 (def handle-size 12)
 (def dash-size 5)
