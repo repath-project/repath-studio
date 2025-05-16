55d52bca740d5b753e82d35d62da27b00ea5864d
Konstantinos Kaloutas <sprocketc@gmail.com>
2025-05-16 17:48:00 +0300

    enhance pan help messages

--- src/renderer/tool/db.cljs
+++ src/renderer/tool/db.cljs
@@ -9,7 +9,7 @@
   [:fn {:error/fn (fn [{:keys [value]} _] (str value " is not a supported tool"))}
    tool?])
 
-(def State [:enum :idle :translate :clone :scale :select :create :edit])
+(def State [:enum :idle :translate :clone :scale :select :create :edit :pan])
 
 (def Cursor
   [:enum
