d6df665a4fbfb7cedc5cbd6042aa43a9b0ba5d32
Konstantinos Kaloutas <sprocketc@gmail.com>
2025-06-12 08:31:46 +0300

    enhance blob tool

--- src/renderer/tool/impl/extension/blob.cljs
+++ src/renderer/tool/impl/extension/blob.cljs
@@ -3,6 +3,8 @@
   (:require
    [clojure.core.matrix :as matrix]
    [renderer.document.handlers :as document.handlers]
+   [renderer.element.handlers :as element.handlers]
+   [renderer.history.handlers :as history.handlers]
    [renderer.tool.handlers :as tool.handlers]
    [renderer.tool.hierarchy :as tool.hierarchy]))
 
@@ -17,27 +19,59 @@
   (matrix/distance (or (:point (:nearest-neighbor db)) (:adjusted-pointer-pos db))
                    (or (:nearest-neighbor-offset db) (:adjusted-pointer-offset db))))
 
+(defn create
+  [db]
+  (let [[offset-x offset-y] (or (:nearest-neighbor-offset db)
+                                (:adjusted-pointer-offset db))
+        radius (pointer-delta db)
+        fill (document.handlers/attr db :fill)
+        stroke (document.handlers/attr db :stroke)]
+    (-> (tool.handlers/set-state db :create)
+        (element.handlers/deselect-all)
+        (element.handlers/add {:type :element
+                               :tag :blob
+                               :attrs {:x (- offset-x radius)
+                                       :y (- offset-y radius)
+                                       :seed (rand-int 1000000)
+                                       :extraPoints 8
+                                       :randomness 4
+                                       :size (* radius 2)
+                                       :fill fill
+                                       :stroke stroke}}))))
+
+(defn update-size
+  [db]
+  (let [[offset-x offset-y] (or (:nearest-neighbor-offset db) (:adjusted-pointer-offset db))
+        radius (pointer-delta db)
+        id (:id (first (element.handlers/selected db)))]
+    (element.handlers/update-el db id #(-> %
+                                           (assoc-in [:attrs :x] (str (- offset-x radius)))
+                                           (assoc-in [:attrs :y] (str (- offset-y radius)))
+                                           (assoc-in [:attrs :size] (str (* radius 2)))))))
+
+(defmethod tool.hierarchy/on-pointer-up :blob
+  [db _e]
+  (if (= (:state db) :create)
+    (-> (tool.handlers/activate db :transform)
+        (history.handlers/finalize "Create blob"))
+    (create db)))
+
+(defmethod tool.hierarchy/on-pointer-move :blob
+  [db _e]
+  (cond-> db
+    (= (:state db) :create)
+    (update-size)))
+
 (defmethod tool.hierarchy/on-drag-start :blob
   [db _e]
-  (let [[offset-x offset-y] (or (:nearest-neighbor-offset db) (:adjusted-pointer-offset db))
-        radius (pointer-delta db)]
-    (tool.handlers/set-temp db {:type :element
-                                :tag :blob
-                                :attrs {:x (- offset-x radius)
-                                        :y (- offset-y radius)
-                                        :seed (rand-int 1000000)
-                                        :extraPoints 8
-                                        :randomness 4
-                                        :size (* radius 2)
-                                        :fill (document.handlers/attr db :fill)
-                                        :stroke (document.handlers/attr db :stroke)}})))
+  (create db))
 
 (defmethod tool.hierarchy/on-drag :blob
   [db _e]
-  (let [[offset-x offset-y] (or (:nearest-neighbor-offset db) (:adjusted-pointer-offset db))
-        radius (pointer-delta db)
-        temp (-> (tool.handlers/temp db)
-                 (assoc-in [:attrs :x] (- offset-x radius))
-                 (assoc-in [:attrs :y] (- offset-y radius))
-                 (assoc-in [:attrs :size] (* radius 2)))]
-    (tool.handlers/set-temp db temp)))
+  (update-size db))
+
+(defmethod tool.hierarchy/on-drag-end :blob
+  [db _e]
+  (-> db
+      (tool.handlers/activate :transform)
+      (history.handlers/finalize "Create blob")))
