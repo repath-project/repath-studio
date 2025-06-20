239fe1a93a96924d7bb87fb2266f0474fbcb3028
Konstantinos Kaloutas <sprocketc@gmail.com>
2025-06-20 13:56:22 +0300

    remove unneeded clj->js

--- src/electron/preload.cljs
+++ src/electron/preload.cljs
@@ -4,17 +4,17 @@
    ["electron" :refer [contextBridge ipcRenderer]]))
 
 (defonce api
-  {;; Strip event as it includes `sender`.
-   ;; https://www.electronjs.org/docs/latest/api/ipc-renderer#ipcrendereronchannel-listener
-   :on (fn [channel f] (.on ipcRenderer channel (fn [_event args] (f args))))
-   :send (fn [channel args] (.send ipcRenderer channel args))
-   :invoke (fn [channel args] (.invoke ipcRenderer channel args))
-   :platform (.-platform js/process)
-   :versions (.-versions js/process)
-   :env (.-env js/process)})
+  #js {;; Strip event as it includes `sender`.
+       ;; https://www.electronjs.org/docs/latest/api/ipc-renderer#ipcrendereronchannel-listener
+       :on (fn [channel f] (.on ipcRenderer channel (fn [_event args] (f args))))
+       :send (fn [channel args] (.send ipcRenderer channel args))
+       :invoke (fn [channel args] (.invoke ipcRenderer channel args))
+       :platform (.-platform js/process)
+       :versions (.-versions js/process)
+       :env (.-env js/process)})
 
 (defn ^:export init!
   "Expose protected methods that allow the renderer process to use the ipcRenderer.
    https://www.electronjs.org/docs/api/context-bridge"
   []
-  (.exposeInMainWorld contextBridge "api" (clj->js api)))
+  (.exposeInMainWorld contextBridge "api" api))
