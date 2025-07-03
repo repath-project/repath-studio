b01d90a69f0c4b45d580e3217822669c7b38e073
Konstantinos Kaloutas <sprocketc@gmail.com>
2025-07-03 18:59:51 +0300

    fix pan on load

--- src/renderer/app/events.cljs
+++ src/renderer/app/events.cljs
@@ -91,7 +91,8 @@
          [[:dispatch [::theme.events/set-document-attr]]
           [:dispatch ^:flush-dom [::set-lang-attrs]]
           ;; We need to render once to get the canvas size right.
-          [:dispatch-later ^:flush-dom {:ms 1 :dispatch [::window.events/update-focused]}]
+          [:dispatch-later ^:flush-dom {:ms 100
+                                        :dispatch [::window.events/update-focused]}]
           [::theme.effects/add-native-listener [::theme.events/set-document-attr]]
           [::effects/ipc-send ["initialized"]]]
          (map (partial vector ::effects/add-listener) document-listeners))}))
