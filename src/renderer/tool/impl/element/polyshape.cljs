fb8a6ca7d07bdc8fd90974cb9c3d0b3783a2979c
Konstantinos Kaloutas <sprocketc@gmail.com>
2025-08-03 08:06:20 +0300

    fix polyshape adjusted point

--- src/renderer/tool/impl/element/polyshape.cljs
+++ src/renderer/tool/impl/element/polyshape.cljs
@@ -49,10 +49,16 @@
                                                       flatten
                                                       (string/join " ")))))
 
+(defn adjusted-point
+  [db point]
+  (let [parent-id (:parent (first (element.handlers/selected db)))
+        [min-x min-y] (element.hierarchy/bbox (element.handlers/entity db parent-id))]
+    (matrix/sub point [min-x min-y])))
+
 (defmethod tool.hierarchy/on-pointer-up ::tool.hierarchy/polyshape
   [db _e]
-  (let [point (or (:point (:nearest-neighbor db))
-                  (:adjusted-pointer-pos db))]
+  (let [point (or (:point (:nearest-neighbor db)) (:adjusted-pointer-pos db))
+        point (cond-> point (= (:state db) :create) (adjusted-point db))]
     (add-point db point)))
 
 (defmethod tool.hierarchy/on-drag-end ::tool.hierarchy/polyshape
@@ -62,9 +68,8 @@
 (defmethod tool.hierarchy/on-pointer-move ::tool.hierarchy/polyshape
   [db _e]
   (let [point (or (:point (:nearest-neighbor db)) (:adjusted-pointer-pos db))
-        {:keys [id parent]} (first (element.handlers/selected db))
-        [min-x min-y] (element.hierarchy/bbox (element.handlers/entity db parent))
-        point (matrix/sub point [min-x min-y])]
+        point (adjusted-point db point)
+        id (:id (first (element.handlers/selected db)))]
     (if (= (:state db) :create)
       (element.handlers/update-attr
        db id :points
