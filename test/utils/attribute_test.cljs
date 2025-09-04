26a3de4c98e8c6be537d1bd102a2b5f272bbf81e
Konstantinos Kaloutas <sprocketc@gmail.com>
2025-09-04 14:52:53 +0300

    fix subscribe outside of reactive context

--- test/utils/attribute_test.cljs
+++ test/utils/attribute_test.cljs
@@ -1,7 +1,8 @@
 (ns utils.attribute-test
   (:require
    [cljs.test :refer-macros [deftest testing are]]
-   [renderer.utils.attribute :as utils.attribute]))
+   [renderer.utils.attribute :as utils.attribute]
+   [renderer.utils.i18n :as i18n]))
 
 (deftest test-str->seq
   (testing "string to sequence conversion"
@@ -36,20 +37,21 @@
                                    :Glyphorientationhorizontal))))
 
 (deftest test-defaults
-  (testing "default tag attributes"
-    (are [x y] (= x y)
-      {:x ""
-       :y ""
-       :rx ""
-       :ry ""
-       :width ""
-       :height ""
-       :fill ""
-       :stroke ""
-       :stroke-dasharray ""
-       :stroke-linejoin ""
-       :style ""
-       :stroke-width ""
-       :opacity ""
-       :id ""
-       :class ""} (utils.attribute/defaults :rect))))
+  (with-redefs [i18n/t (fn [& _] "translation")]
+    (testing "default tag attributes"
+      (are [x y] (= x y)
+        {:x ""
+         :y ""
+         :rx ""
+         :ry ""
+         :width ""
+         :height ""
+         :fill ""
+         :stroke ""
+         :stroke-dasharray ""
+         :stroke-linejoin ""
+         :style ""
+         :stroke-width ""
+         :opacity ""
+         :id ""
+         :class ""} (utils.attribute/defaults :rect)))))
