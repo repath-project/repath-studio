dfa1ee241f38c48eb2d1852fad9c2aab89a71077
Konstantinos Kaloutas <sprocketc@gmail.com>
2025-09-05 17:56:34 +0300

    preview fill color on pointer over element

--- src/renderer/tool/impl/misc/fill.cljs
+++ src/renderer/tool/impl/misc/fill.cljs
@@ -5,6 +5,7 @@
    [renderer.history.handlers :as history.handlers]
    [renderer.tool.handlers :as tool.handlers]
    [renderer.tool.hierarchy :as tool.hierarchy]
+   [renderer.utils.element :as utils.element]
    [renderer.utils.i18n :refer [t]]))
 
 (derive :fill ::tool.hierarchy/tool)
@@ -28,3 +29,15 @@
         el-id (-> e :element :id)]
     (-> (element.handlers/set-attr db el-id :fill color)
         (history.handlers/finalize #(t [::fill "Fill"])))))
+
+(defmethod tool.hierarchy/on-pointer-move :fill
+  [db e]
+  (let [color (document.handlers/attr db :fill)
+        el (-> e :element)]
+    (cond-> (history.handlers/reset-state db)
+      (not (utils.element/root? el))
+      (element.handlers/set-attr (:id el) :fill color))))
+
+(defmethod tool.hierarchy/on-pointer-down :fill
+  [db _e]
+  (history.handlers/reset-state db))
