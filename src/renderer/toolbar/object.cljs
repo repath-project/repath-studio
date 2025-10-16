ccb290f398c540cca3c7d22593c017785434efdf
Konstantinos Kaloutas <sprocketc@gmail.com>
2025-10-16 13:07:53 +0300

    add more object actions button

--- src/renderer/toolbar/object.cljs
+++ src/renderer/toolbar/object.cljs
@@ -1,126 +1,150 @@
 (ns renderer.toolbar.object
   (:require
+   ["@radix-ui/react-dropdown-menu" :as DropdownMenu]
    [re-frame.core :as rf]
    [renderer.element.events :as-alias element.events]
    [renderer.element.subs :as-alias element.subs]
+   [renderer.element.views :as element.views]
    [renderer.toolbar.views :as toolbar.views]
-   [renderer.utils.i18n :refer [t]]))
+   [renderer.utils.i18n :refer [t]]
+   [renderer.views :as views]
+   [renderer.window.subs :as-alias window.subs]))
 
 (defn index-actions
   [disabled]
-  [{:title (t [::bring-front "Bring to front"])
+  [{:label (t [::bring-front "Bring to front"])
     :icon "bring-front"
     :action [::element.events/raise-to-top]
     :disabled disabled}
-   {:title (t [::send-back "Send to back"])
+   {:label (t [::send-back "Send to back"])
     :icon "send-back"
     :action [::element.events/lower-to-bottom]
     :disabled disabled}
-   {:title (t [::bring-forward "Bring forward"])
+   {:label (t [::bring-forward "Bring forward"])
     :icon "bring-forward"
     :action [::element.events/raise]
     :disabled disabled}
-   {:title (t [::send-backward "Send backward"])
+   {:label (t [::send-backward "Send backward"])
     :icon "send-backward"
     :action [::element.events/lower]
     :disabled disabled}])
 
 (defn group-actions
   [disabled]
-  [{:title (t [::group "Group"])
+  [{:label (t [::group "Group"])
     :icon "group"
     :disabled disabled
     :action [::element.events/group]}
-   {:title (t [::ungroup "Ungroup"])
+   {:label (t [::ungroup "Ungroup"])
     :icon "ungroup"
     :disabled disabled
     :action [::element.events/ungroup]}])
 
 (defn alignment-actions
   [disabled]
-  [{:title (t [::align-left "Align left"])
+  [{:label (t [::align-left "Align left"])
     :icon "objects-align-left"
     :disabled disabled
     :action [::element.events/align :left]}
-   {:title (t [::align-center-hor "Align center horizontally"])
+   {:label (t [::align-center-hor "Align center horizontally"])
     :disabled disabled
     :icon "objects-align-center-horizontal"
     :action [::element.events/align :center-horizontal]}
-   {:title (t [::align-right "Align right"])
+   {:label (t [::align-right "Align right"])
     :icon "objects-align-right"
     :disabled disabled
     :action [::element.events/align :right]}
    {:type :divider}
-   {:title (t [::align-top "Align top"])
+   {:label (t [::align-top "Align top"])
     :icon "objects-align-top"
     :disabled disabled
     :action [::element.events/align :top]}
-   {:title (t [::align-center-ver "Align center vertically"])
+   {:label (t [::align-center-ver "Align center vertically"])
     :icon "objects-align-center-vertical"
     :disabled disabled
     :action [::element.events/align :center-vertical]}
-   {:title (t [::align-bottom "Align bottom"])
+   {:label (t [::align-bottom "Align bottom"])
     :icon "objects-align-bottom"
     :disabled disabled
     :action [::element.events/align :bottom]}])
 
 (defn boolean-actions
   [disabled]
-  [{:title (t [::unite "Unite"])
+  [{:label (t [::unite "Unite"])
     :icon "unite"
     :disabled disabled
     :action [::element.events/boolean-operation :unite]}
-   {:title (t [::intersect "Intersect"])
+   {:label (t [::intersect "Intersect"])
     :icon "intersect"
     :disabled disabled
     :action [::element.events/boolean-operation :intersect]}
-   {:title (t [::subtract "Subtract"])
+   {:label (t [::subtract "Subtract"])
     :icon "subtract"
     :disabled disabled
     :action [::element.events/boolean-operation :subtract]}
-   {:title (t [::exclude "Exclude"])
+   {:label (t [::exclude "Exclude"])
     :icon "exclude"
     :disabled disabled
     :action [::element.events/boolean-operation :exclude]}
-   {:title (t [::divide "Divide"])
+   {:label (t [::divide "Divide"])
     :icon "divide"
     :disabled disabled
     :action [::element.events/boolean-operation :divide]}])
 
 #_(defn distribute-actions
     []
-    [{:title "Distribute spacing horizontally"
+    [{:label "Distribute spacing horizontally"
       :icon "distribute-spacing-horizontal"
       :disabled true
       :action [::element.events/istribute-spacing :horizontal]}
-     {:title "Distribute spacing vertically"
+     {:label "Distribute spacing vertically"
       :icon "distribute-spacing-vertical"
       :disabled true
       :action [::element.events/distribute-spacing :vertical]}])
 
 #_(defn rotate-actions
     []
-    [{:title "Rotate 90° clockwise"
+    [{:label "Rotate 90° clockwise"
       :icon "rotate-clockwise"
       :disabled true
       :action [::element.events/rotate -90]}
-     {:title "Rotate 90° counterclockwise"
+     {:label "Rotate 90° counterclockwise"
       :icon "rotate-counterclockwise"
       :disabled true
       :action [::element.events/rotate 90]}])
 
 #_(defn flip-actions
     []
-    [{:title "Flip horizontally"
+    [{:label "Flip horizontally"
       :icon "flip-horizontal"
       :disabled true
       :action [::element.events/flip :horizontal]}
-     {:title "Flip vertically"
+     {:label "Flip vertically"
       :icon "flip-vertical"
       :disabled true
       :action [::element.events/flip :vertical]}])
 
-(defn root
+(defn more-button
+  [disabled]
+  [:> DropdownMenu/Root
+   [:> DropdownMenu/Trigger
+    {:as-child true}
+    [:button.button.flex.items-center.justify-center.px-2.font-mono.rounded
+     {:aria-label "More object actions"
+      :disabled disabled}
+     [views/icon "ellipsis-h"]]]
+   [:> DropdownMenu/Portal
+    (->> (element.views/context-menu)
+         (map views/dropdown-menu-item)
+         (into [:> DropdownMenu/Content
+                {:side "left"
+                 :align "end"
+                 :class "menu-content rounded-sm"
+                 :on-key-down #(.stopPropagation %)
+                 :on-escape-key-down #(.stopPropagation %)}
+                [:> DropdownMenu/Arrow {:class "fill-primary"}]]))]])
+
+(defn object-buttons
   []
   (let [some-selected? @(rf/subscribe [::element.subs/some-selected?])
         multiple-selected? @(rf/subscribe [::element.subs/multiple-selected?])
@@ -132,5 +156,15 @@
     (->> object-actions
          (interpose [{:type :divider}])
          (flatten)
-         (map toolbar.views/button)
-         (into [:div.flex.flex-col.text-center.flex-0.toolbar]))))
+         (map toolbar.views/button))))
+
+(defn root
+  []
+  (let [some-selected? @(rf/subscribe [::element.subs/some-selected?])
+        md? @(rf/subscribe [::window.subs/breakpoint? :md])]
+    [:div.flex.flex-col.text-center.flex-0.toolbar
+     (into [:<>] (object-buttons))
+     (when-not md?
+       [:<>
+        [:span.h-divider]
+        [more-button (not some-selected?)]])]))
