f1905eab3dae4a09d859accec14687101a5a5d8b
Konstantinos Kaloutas <sprocketc@gmail.com>
2025-10-17 09:06:29 +0300

    install capacitor and update assets

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'studio.repath.app',
+  appName: 'Repath Studio',
+  webDir: 'resources/public'
+};
+
+export default config;
