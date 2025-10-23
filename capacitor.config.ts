55e0247720dc2a8873ac9401bd84d6de0ccd38db
Konstantinos Kaloutas <sprocketc@gmail.com>
2025-10-23 11:19:24 +0300

    add capacitor splash screen plugin

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,15 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'studio.repath.app',
   appName: 'Repath Studio',
-  webDir: 'resources/public'
+  webDir: 'resources/public',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2500,
+      launchAutoHide: true,
+      launchFadeOutDuration: 250,
+      backgroundColor: "#DD286C",
+    },
+  }
 };
 
 export default config;
